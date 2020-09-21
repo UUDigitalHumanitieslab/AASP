a1a2b56523e8ad0bf2165bfd6fea2152bcab11d3
BeritJanssen <berit.janssen@gmail.com>
2020-09-21 16:43:53 +0200

    dot instead of source

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@ python manage.py flush --no-input
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
 
-source ../../../private/envvars
+. ../../../private/envvars
 
 gunicorn --bind 0.0.0.0:8000 --workers 4 aasp.wsgi
