3f5ee743d2d9db4277b9d2b4de85456dfc4edb93
BeritJanssen <berit.janssen@gmail.com>
2021-11-17 17:01:08 +0100

    added submit button

--- files/forms.py
+++ files/forms.py
@@ -2,6 +2,5 @@ from django import forms
 
 class SpeakerDirectoryForm(forms.Form):
     speaker = forms.CharField(required=True)
-    directory = forms.FileField(widget=forms.ClearableFileInput(attrs=
-        {'multiple': True, 
-        'onchange':'speakerdirectoryform.submit();'}))
+    files = forms.FileField(widget=forms.ClearableFileInput(attrs=
+        {'multiple': True}))
